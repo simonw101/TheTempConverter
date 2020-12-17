99e4182fac81b5715ff04d708215bb8f9d11b632
simonw101a <simonw101@googlemail.com>
2020-12-17 13:52:35 +0000

    lecture 40

--- app/src/main/java/com/example/thetempconverter/MainActivity.kt
+++ app/src/main/java/com/example/thetempconverter/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.thetempconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.thetempconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun toCelcius(view: View) {
+
+
+
+    }
+
+    fun toFahrenheit(view: View) {
+
+
+
     }
 }
